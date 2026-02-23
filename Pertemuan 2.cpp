95bee52dd68484581ab226fdc5faf62db99ce049
Khazz <thelenaid@gmail.com>
2026-02-23 20:59:40 +0700

    menambahkan library

---
+++ Pertemuan 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){ //mulai
+    int p, l, luas;
+
+    //langkah 2
+    cout << "Masukan Panjang : ";
+    cin >> p;
+
+    //langkah 3
+    cout << "Masukan Lebar : ";
+    cin >> l;
+    
+}
